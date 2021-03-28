d8731aae73711c99eccfb9233733f2ac0833ccc4
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-03-28 21:03:18 +0100

    Add services folder

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+//get data from api
+const getDataFromApi = () => {
+  return fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) => {
+      return data.results;
+    });
+};
+
+export default {
+  getDataFromApi: getDataFromApi,
+};
