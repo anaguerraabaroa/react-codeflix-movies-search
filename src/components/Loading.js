f0edbbdd8102c5ab26312d4c2a3826e6d9791102
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-02 23:47:43 +0100

    Add loading component and css styles

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../stylesheets/components/_loading.scss";
+
+const Loading = () => {
+  return (
+    <section className="loading__wrapper">
+      <div className="loading"></div>;
+    </section>
+  );
+};
+
+export default Loading;
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -2,17 +2,21 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import apiDetail from "../services/api-details";
 import defaultImage from "../images/default_image.jpg";
+import Loading from "../components/Loading";
 import PropTypes from "prop-types";
 import "../stylesheets/layout/_movie-details.scss";
 
 const MovieDetails = (props) => {
   // state
   const [movieDetails, setMovieDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // lifecycle: API request to get movie details when component mounts
   useEffect(() => {
+    setIsLoading(true);
     apiDetail.getDetailsFromApi(props).then((movie) => {
       setMovieDetails(movie);
+      setIsLoading(false);
     });
   }, [props]);
 
@@ -26,50 +30,62 @@ const MovieDetails = (props) => {
   };
 
   return (
-    <section className="card__details">
-      <article className="details">
-        <img
-          className="details__image"
-          src={Image()}
-          alt={movieDetails.Title}
-          title={movieDetails.Title}
-        />
-        <div className="details__wrapper">
-          <h2 className="details__title">{movieDetails.Title}</h2>
-          <p className="details__data">
-            <i
-              className="details__icon far fa-calendar-alt"
-              aria-hidden="true"
-            ></i>
-            <span className="details__highlight">Year: </span>
-            {movieDetails.Year}
-          </p>
-          <p className="details__data">
-            <i className="details__icon fas fa-users" aria-hidden="true"></i>
-            <span className="details__highlight">Actors: </span>
-            {movieDetails.Actors}
-          </p>
-          <p className="details__data">
-            <i className="details__icon fas fa-video" aria-hidden="true"></i>
-            <span className="details__highlight">Plot: </span>
-            {movieDetails.Plot}
-          </p>
-          <p className="details__metascore">
-            <i className="details__icon fas fa-star" aria-hidden="true"></i>
-            <span className="details__highlight">Metascore: </span>
-            {movieDetails.Metascore}
-          </p>
-          <Link
-            className="details__btn"
-            to="/"
-            title="Go back to movies list"
-            aria-label="Go back to movies list"
-          >
-            Return
-          </Link>
-        </div>
-      </article>
-    </section>
+    <>
+      {isLoading === true ? (
+        <Loading />
+      ) : (
+        <section className="card__details">
+          <article className="details">
+            <img
+              className="details__image"
+              src={Image()}
+              alt={movieDetails.Title}
+              title={movieDetails.Title}
+            />
+            <div className="details__wrapper">
+              <h2 className="details__title">{movieDetails.Title}</h2>
+              <p className="details__data">
+                <i
+                  className="details__icon far fa-calendar-alt"
+                  aria-hidden="true"
+                ></i>
+                <span className="details__highlight">Year: </span>
+                {movieDetails.Year}
+              </p>
+              <p className="details__data">
+                <i
+                  className="details__icon fas fa-users"
+                  aria-hidden="true"
+                ></i>
+                <span className="details__highlight">Actors: </span>
+                {movieDetails.Actors}
+              </p>
+              <p className="details__data">
+                <i
+                  className="details__icon fas fa-video"
+                  aria-hidden="true"
+                ></i>
+                <span className="details__highlight">Plot: </span>
+                {movieDetails.Plot}
+              </p>
+              <p className="details__metascore">
+                <i className="details__icon fas fa-star" aria-hidden="true"></i>
+                <span className="details__highlight">Metascore: </span>
+                {movieDetails.Metascore}
+              </p>
+              <Link
+                className="details__btn"
+                to="/"
+                title="Go back to movies list"
+                aria-label="Go back to movies list"
+              >
+                Return
+              </Link>
+            </div>
+          </article>
+        </section>
+      )}
+    </>
   );
 };
 
